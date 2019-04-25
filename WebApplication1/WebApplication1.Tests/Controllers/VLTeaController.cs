a0420becbe58222c067b7d630b5e56385581b560
thaoduong1199 <thaoduong1199@gmail.com>
2019-04-25 15:14:08 +0700

    MVC_TEST

---
+++ WebApplication1/WebApplication1.Tests/Controllers/VLTeaController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Models;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using WebApplication1.Controllers;
+
+namespace WebApplication1.Tests.Controllers
+{
+    [TestClass]
+    public class VLTeaController
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var db = new CS4PEEntities();
+            var controller = new BubbleTeaController();
+            var result = controller.Index();
+
+            var view = result as ViewResult;
+            Assert.IsNotNull(view);
+
+            var model = view.Model as List<BubleTea>;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(db.BubleTeas.Count(), model.Count);
+        }
+        [TestMethod]
+        public void TestDetails()
+        {
+            var db = new CS4PEEntities();
+            var item = db.BubleTeas.First();
+            var controller = new BubbleTeaController();
+
+            var result = controller.Details(item.id);
+            var view = result as ViewResult;
+            Assert.IsNotNull(view);
+            var model = view.Model as BubleTea;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(item.id, model.id);
+
+            result = controller.Details(0);
+            Assert.IsInstanceOfType(result, typeof(HttpNotFoundResult));
+
+
+        }
+        [TestMethod]
+        public void TestCreate()
+        {
+
+
+        }
+        [TestMethod]
+        public void TestCreateP()
+        {
+            var db = new CS4PEEntities();
+            var model = new BubleTea
+            {
+                Name = "Tra sua VL",
+                Price = 26000,
+                Topping = "tran chau trang"
+            };
+            var controller = new BubbleTeaController();
+
+            var result = controller.Create(model);
+            var redirect = result as RedirectToRouteResult;
+            Assert.IsNotNull(redirect);
+            Assert.AreEqual("Index", redirect.RouteValues["action"]);
+            var item = db.BubleTeas.Find(model.id);
+            Assert.IsNotNull(item);
+            Assert.AreEqual(model.Name, item.Name);
+            Assert.AreEqual(model.Price, item.Price);
+            Assert.AreEqual(model.Topping, item.Topping);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/BubbleTeaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BubbleTeaController : Controller
+    {
+        private CS4PEEntities db = new CS4PEEntities();
+
+        // GET: /BubbleTea/
+        public ActionResult Index()
+        {
+            return View(db.BubleTeas.ToList());
+        }
+
+        // GET: /BubbleTea/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BubleTea bubletea = db.BubleTeas.Find(id);
+            if (bubletea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bubletea);
+        }
+
+        // GET: /BubbleTea/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /BubbleTea/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="id,Name,Price,Topping")] BubleTea bubletea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BubleTeas.Add(bubletea);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bubletea);
+        }
+
+        // GET: /BubbleTea/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BubleTea bubletea = db.BubleTeas.Find(id);
+            if (bubletea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bubletea);
+        }
+
+        // POST: /BubbleTea/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="id,Name,Price,Topping")] BubleTea bubletea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bubletea).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bubletea);
+        }
+
+        // GET: /BubbleTea/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BubleTea bubletea = db.BubleTeas.Find(id);
+            if (bubletea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bubletea);
+        }
+
+        // POST: /BubbleTea/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BubleTea bubletea = db.BubleTeas.Find(id);
+            db.BubleTeas.Remove(bubletea);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
